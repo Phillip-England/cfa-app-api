b94f66b01eceab5b0f79d3518fd1935489252621
Phillip-England <Phillip.mark.england@gmail.com>
2023-05-02 05:42:37 -0500

    shorthanded all routes to req and res

--- routes/user_routes/delete_current_user.py
+++ routes/user_routes/delete_current_user.py
@@ -5,18 +5,18 @@ from db import Mongo
 from db import delete_all
 from db import delete_one
 from middleware import auth_middleware
+from lib import respond
 
 
 def delete_current_user(app: FastAPI, mongo: Mongo):
 
     @app.delete("/user")
-    async def route_delete_current_user(request: Request, response: Response):
+    async def route_delete_current_user(req: Request, res: Response):
 
         # getting the logged in user
-        user, err = auth_middleware(request, mongo)
+        user, err = auth_middleware(req, mongo)
         if err != None:
-            response.status_code = 400
-            return {'msg': 'unauthorized'}
+            respond(res, 'unauthorized', 400)
 
         # dropping the user
         result = delete_one(mongo.users, {'_id': user['_id']})
@@ -26,7 +26,7 @@ def delete_current_user(app: FastAPI, mongo: Mongo):
         result = delete_all(mongo.stores, {'user': user['_id']})
 
         # resetting session cookies
-        response.set_cookie(
+        res.set_cookie(
             key='session_token',
             value=''
         )
